d9b4936dc0ecbe53b29defd6acdca0a1464f059f
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-11-22 20:26:42 +0400

    refresh added

--- app/index.js
+++ app/index.js
@@ -10,10 +10,12 @@ import {
   TouchableOpacity,
   StyleSheet,
   TouchableWithoutFeedback,
+  Dimensions,
   Keyboard,
   Image,
   ScrollView,
   ImageBackground,
+  RefreshControl,
 } from "react-native";
 import * as Location from "expo-location";
 
@@ -22,8 +24,9 @@ export default function App() {
     require("../assets/images/sunny.png")
   );
   const [location, setLocation] = useState(null);
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
 
   const getLocation = async () => {
     try {
@@ -40,40 +43,50 @@ export default function App() {
     }
   };
 
+  const getWeatherData = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=121644eb7e360359ae4457fdf296252f`
+      );
+      setWeatherData(data.data);
+      console.log(data.data.list[0].main.feels_like);
+      setRefreshing(false);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   useEffect(() => {
     getLocation();
+    location ? getWeatherData() : null;
   }, []);
 
-  useEffect(() => {
-    const getWeatherData = async () => {
-      try {
-        const data = await axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=121644eb7e360359ae4457fdf296252f`
-        );
-        setWeatherData(data.data);
-        console.log(data.data.list[0].main.feels_like);
-      } catch (error) {
-        alert(error);
-      }
-    };
-    location ? getWeatherData() : null;
-  }, [location]);
-
-  // weatherData.list.map((e) => {
-  //   console.log(e.weather);
-  // });
+  const onRefresh = () => {
+    setRefreshing(true);
+    getWeatherData();
+  };
 
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          tintColor="#3F51B5"
+          title="Yenileniyor..."
+        />
+      }
+    >
       <ImageBackground
         source={require("../assets/images/sunny.png")}
         style={styles.background}
       >
         <View style={styles.container}>
-          <Text style={{}}>
-            {weatherData?.city?.country}, {weatherData?.city?.name}
+          <Text style={{ fontSize: 50 }}>
+            {/* {weatherData?.city?.country}, */}
+            {weatherData?.city?.name}
           </Text>
-          <Text style={{}}>
+          <Text style={{ fontSize: 30 }}>
             {(weatherData?.list[0]?.main?.temp - 273.15).toFixed()}°C
           </Text>
           <Text style={styles.weatherDescription}>
@@ -92,29 +105,20 @@ export default function App() {
         </View>
       </ImageBackground>
     </ScrollView>
-    // <View style={styles.container}>
-    //   <Image
-    //   source={{
-    //     uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFBr35nsGltX_wIDUpo4TCQCXGHsnU1P9qUQ&usqp=CAU",
-    //   }}
-    //   style={{ width: "100%", height: 1500, resizeMode: "cover" }}
-    // />
-    //   <Text>Name: test</Text>
-    // </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: "stretch",
-    justifyContent: "center",
-  },
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 180,
+    paddingTop: 80,
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
   weatherDescription: {
     textTransform: "capitalize",
